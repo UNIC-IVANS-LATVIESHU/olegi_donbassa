7edb6cd98b7bab4def3f6d0462b3e5c677d00669
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-09-02 21:34:43 +0300

    Added POST "/createuser"
    NOT TESTED YET
    
    Летом карлики находятся в тени общества

--- src/server.js
+++ src/server.js
@@ -89,7 +89,50 @@ function checkintegrationsettings() {
   // ******************************************
 }
 
-app.get("/", function (req, res) {
+/** This function generates a random password. This function requires "lodash" library
+ * @param  {number} length=8 The length of the password to be generated
+ * @param  {boolean} add_dashes=false This parameter selects whether the password should be separated by a sign - or not
+ * @param  {string} available_sets="luds" This string selects which characters the password should include
+ * @return {string} Returns generated random password
+ */
+function random_str(length = 8, add_dashes = false, available_sets = "luds") {
+  // Not http
+  var sets = [];
+  if (available_sets.indexOf("l") >= 0) {
+    sets.push("abcdefghjkmnpqrstuvwxyz");
+  }
+  if (available_sets.indexOf("u") >= 0) {
+    sets.push("ABCDEFGHJKMNPQRSTUVWXYZ");
+  }
+  if (available_sets.indexOf("d") >= 0) {
+    sets.push("123456789");
+  }
+  if (available_sets.indexOf("s") >= 0) {
+    sets.push("!@#$%*?");
+  }
+  var all = "";
+  var password = "";
+  sets.forEach((set) => {
+    password += set[Math.floor(Math.random() * set.length)];
+    all += set;
+  });
+  all = all.split("");
+  for (var i = 0; i < length - sets.length; i++)
+    password += all[Math.floor(Math.random() * all.length)];
+  password = _.shuffle(password) + "";
+  password = password.replace(/,/g, "");
+  if (!add_dashes) return password;
+  var dash_len = Math.floor(Math.sqrt(length));
+  var dash_str = "";
+  while (password.length > dash_len) {
+    dash_str += password.substr(0, dash_len) + "-";
+    password = password.substr(dash_len);
+  }
+  dash_str += password;
+  return dash_str;
+}
+
+app.get("/checkifuserexist", function (req, res) {
   let status = JSON.parse(checkintegrationsettings());
   // If status does not contain any information send an error
   if (!status) {
@@ -128,12 +171,92 @@ app.get("/", function (req, res) {
       );
     }
   }
-}); // This responds a POST request for the homepage
+});
 
-app.post("/", function (req, res) {
-  console.log("Got a POST request for the homepage");
-  res.send("Hello POST");
-}); // This responds a DELETE request for the /del_user page.
+app.post("/createuser", function (req, res) {
+  // POST request
+  let status = JSON.parse(checkintegrationsettings());
+  // If status does not contain any information send an error
+  if (!status) {
+    res.send(
+      JSON.stringify({
+        status: error,
+        message: "Service disabled or not properly configured!",
+      })
+    );
+  } else {
+    checkexist = JSON.parse(checkifuserexist(req.user["email"]));
+    if (checkexist.status) {
+      if (checkexist.exist) {
+        res.send(
+          JSON.stringify({
+            status: "success",
+            new_user: false,
+            user_id: checkexist.user_id,
+            message: "User Already Exist in moodle",
+          })
+        );
+      } else {
+        //Creating a link for a moodle webservice
+        let paramStr =
+          "/webservice/rest/server.php?wstoken=" + md_client_using.moodle_token;
+        paramStr +=
+          "&wsfunction=core_user_create_users&users[0][username]=" +
+          encodeURI(req.user["email"].toLowerCase());
+        paramStr +=
+          "&users[0][password]=" +
+          encodeURI(req.user["password"]) +
+          "&users[0][email]=" +
+          encodeURI(req.user["email"]);
+        paramStr +=
+          "&users[0][firstname]=" +
+          encodeURI(req.user["name"]) +
+          "&users[0][lastname]=" +
+          encodeURI(req.user["last"]);
+        paramStr +=
+          "&users[0][customfields][0][type]=programid&users[0][customfields][0][value]=IFF";
+        paramStr += "&moodlewsrestformat=json";
+        // Preparing result with all users data taken from a moodle
+        let result = JSON.parse(get(paramStr));
+        //Checking for an error while creating a user with error detecting algorithm. If there is no error => creating a user
+        if (result) {
+          if (result.exception) {
+            res.send(
+              JSON.stringify({
+                status: "error",
+                message: `Error on Creating user: ${result.errorcode}`,
+              })
+            );
+          } else {
+            moodle_user_id = result[0].id;
+            res.send(
+              JSON.stringify({
+                status: "success",
+                new_user: true,
+                user_id: moodle_user_id,
+                message: "User created",
+              })
+            );
+          }
+        } else {
+          res.send(
+            JSON.stringify({
+              status: "error",
+              message: "Error on creating the user",
+            })
+          );
+        }
+      }
+    } else {
+      res.send(
+        JSON.stringify({
+          status: "error",
+          message: checkexist.message,
+        })
+      );
+    }
+  }
+});
 
 app.patch("/squares/:x/:y/paint", (req, res, next) => {
   const x = req.params.x;
