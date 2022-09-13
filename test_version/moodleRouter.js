a5155fdd93cd2b5051092db95a0ccf9d839ae393
RAYNingTime <ivan0kosyakov@gmail.com>
2022-09-13 11:56:01 +0300

    Working /enroltocourse request

--- test_version/moodleRouter.js
+++ test_version/moodleRouter.js
@@ -8,9 +8,8 @@ router.route("/createuser").post(
       "/webservice/rest/server.php?wstoken=" +
       process.env.MOODLE_TOKEN +
       "&wsfunction=core_user_get_users&criteria[0][key]=email&criteria[0][value]=" +
-      encodeURI(req.body.user["email"]) +
+      encodeURI(req.body["email"]) +
       "&moodlewsrestformat=json";
-
     axios
       .get(process.env.MOODLE_URL + theUrl)
       .then((result) => {
@@ -29,82 +28,176 @@ router.route("/createuser").post(
 
         if (checkexist.status) {
           if (checkexist.exist) {
-            res.JSON({
-              status: true,
-              new_user: false,
-              user_id: checkexist.user_id,
-              message: "User Already Exist in moodle",
-            });
+            res.send(
+              JSON.stringify({
+                status: true,
+                new_user: false,
+                user_id: checkexist.user_id,
+                message: "User Already Exist in moodle",
+              })
+            );
           } else {
             //Creating a link for a moodle webservice
             var newUrl =
               "/webservice/rest/server.php?wstoken=" + process.env.MOODLE_TOKEN;
             newUrl +=
               "&wsfunction=core_user_create_users&users[0][username]=" +
-              encodeURI(req.body.user["email"].toLowerCase());
+              encodeURI(req.body["email"].toLowerCase());
             newUrl +=
               "&users[0][password]=" +
-              encodeURI(req.body.user["password"]) +
+              encodeURI(req.body["password"]) +
               "&users[0][email]=" +
-              encodeURI(req.body.user["email"]);
+              encodeURI(req.body["email"]);
             newUrl +=
               "&users[0][firstname]=" +
-              encodeURI(req.body.user["name"]) +
+              encodeURI(req.body["name"]) +
               "&users[0][lastname]=" +
-              encodeURI(req.body.user["last"]);
+              encodeURI(req.body["last"]);
             newUrl +=
               "&users[0][customfields][0][type]=programid&users[0][customfields][0][value]=IFF";
             newUrl += "&moodlewsrestformat=json";
 
             axios
               .get(process.env.MOODLE_URL + newUrl)
-              .then((main_result) => {
-                if (main_result.data.exception) {
+              .then((response) => {
+                if (response.data.exception) {
                   res.send(
                     JSON.stringify({
                       status: false,
-                      message: `Error on Creating user: ${main_result.errorcode}`,
+                      message: `Error on Creating user: ${response.errorcode} ${response.message}`,
+                      data: req.body,
                     })
                   );
-
-                  res.JSON({
-                    status: false,
-                    message: `Error on Creating user: ${main_result.errorcode} ${main_result.message}`,
-                    data: req.body,
-                  });
                 } else {
                   res.send(
                     JSON.stringify({
                       status: true,
                       new_user: true,
-                      user_id: main_result.data[0].id,
+                      user_id: response.data[0].id,
                       message: "User created",
                     })
                   );
                 }
               })
               .catch((error) => {
-                res.JSON({
-                  status: false,
-                  message: error.message,
-                  data: req.body,
-                });
+                res.send(
+                  JSON.stringify({
+                    status: false,
+                    message: error.message,
+                    data: req.body,
+                  })
+                );
               });
           }
         } else {
-          res.JSON({
-            status: false,
-            message: error.message,
-            data: req.body,
-          });
+          res.send(
+            JSON.stringify({
+              status: false,
+              message: checkexist.message, // ? FIXME: Not sure about checkexist.message
+              data: req.body,
+            })
+          );
         }
       })
       .catch((error) => {
-        res.JSON({
-          status: false,
-          message: error.message,
-          data: req.body,
-        });
+        res.send(
+          JSON.stringify({
+            status: false,
+            message: error.message,
+            data: req.body,
+          })
+        );
+      });
+  })
+);
+
+router.route("/enroltocourse").post(
+  (enroltocourse = (req, res) => {
+    // req.body.user["password"] = random_str(); // ! TODO: Create functional function
+
+    axios
+      .post("http://localhost:3030/createuser", req.body.user) // ! TODO: Change the URL of the request
+      .then(function (response) {
+        if (response.data.status) {
+          //Creating a link for a moodle webservice
+          let newUrl =
+            "/webservice/rest/server.php?wstoken=" + process.env.MOODLE_TOKEN;
+          newUrl +=
+            "&wsfunction=enrol_manual_enrol_users&moodlewsrestformat=json&enrolments[0][roleid]=5&enrolments[0][userid]=" +
+            response.data.user_id;
+          newUrl += "&enrolments[0][courseid]=" + req.body.course_id;
+          // Preparing result with all data taken from a moodle
+          axios
+            .get(process.env.MOODLE_URL + newUrl)
+            .then((result) => {
+              //Checking for an error while enrolling to a course with error detecting algorithm. If there is no error => enrolling to a course
+              if (result) {
+                if (result.hasOwnProperty("exception")) {
+                  res.send(
+                    JSON.stringify({
+                      status: false,
+                      message: result.message, // ? FIXME: Not sure if it gonna have a message about error, so should test it
+                      data: req.body,
+                    })
+                  );
+                } else {
+                  if (response.data.new_user) {
+                    // self(new_user_email(req.body.user, req.body.product_details)); // ! FIXME: new_user_email Should be created in the PHP part of the code
+                    res.send(
+                      JSON.stringify({
+                        status: true,
+                        message: "User Created and Enrolled",
+                      })
+                    );
+                  } else {
+                    // self(existing_user_email(req.body.user, req.body.product_details)); // ! FIXME: existing_user_email Should be created in the PHP part of the code
+                    res.send(
+                      JSON.stringify({
+                        status: true,
+                        message: "User Enrolled",
+                      })
+                    );
+                  }
+                }
+              } else {
+                res.send(
+                  JSON.stringify({
+                    status: false,
+                    message:
+                      "Error on enrolling the user into the course." +
+                      result.message, // ? FIXME: Not sure if it gonna have a message about error, so should test it
+                    data: req.body,
+                  })
+                );
+              }
+            })
+            .catch((error) => {
+              res.send(
+                JSON.stringify({
+                  status: false,
+                  message: error.message,
+                  data: req.body,
+                })
+              );
+            });
+        } else {
+          res.send(
+            JSON.stringify({
+              status: false,
+              message: response.message,
+              data: req.body,
+            })
+          );
+        }
+      })
+      .catch(function (error) {
+        res.send(
+          JSON.stringify({
+            status: false,
+            message: error.message,
+            data: req.body,
+          })
+        );
       });
   })
 );
