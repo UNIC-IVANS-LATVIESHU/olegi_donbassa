37d64337fb53e50ca2f2bfb4ab4e44c875fe75ed
RAYNingTime <ivan0kosyakov@gmail.com>
2022-09-20 14:25:28 +0300

    Test for /createuser and /enrolltocourse

--- test/moodleRouter.test.js
+++ test/moodleRouter.test.js
@@ -8,63 +8,6 @@ const app = express();
 chai.use(chaiHttp);
 app.use(express.urlencoded({ extended: false }));
 
-// describe("/POST creat user", () => {
-//   it("it should create a user", (done) => {
-//     chai
-//       .request(server)
-//       .post("/createuser")
-//       .send({
-//         email: "test@unic.ac.cy",
-//         password: "tE5|tE5|",
-//         name: "Test",
-//         last: "Test",
-//       })
-//       .end((err, res) => {
-//         chai.expect(err).to.be.null;
-//         chai.expect(res).to.have.status(201);
-
-//         const answer = JSON.parse(res.text);
-//         if (answer.user_id) {
-//           const theLink =
-//             "/webservice/rest/server.php?wstoken=" +
-//             process.env.DELETE_USER_TOKEN +
-//             "&wsfunction=core_user_delete_users&moodlewsrestformat=json&userids[0]=" +
-//             answer.user_id;
-
-//           axios.delete(process.env.MOODLE_URL + theLink);
-//         }
-
-//         done();
-//       });
-//   });
-
-//   it("if user already exists", (done) => {
-//     chai
-//       .request(server)
-//       .post("/createuser")
-//       .send({
-//         email: "ivan0kosyakov@gmail.com",
-//         password: "ivanIVAN1234@",
-//         name: "Ivan",
-//         last: "Ivan",
-//       })
-//       .end((err, res) => {
-//         chai.expect(err).to.be.null;
-//         chai.expect(res).to.have.status(200);
-//         chai.expect(res.text).to.equal(
-//           JSON.stringify({
-//             status: true,
-//             new_user: false,
-//             user_id: 20,
-//             message: "User Already Exist in moodle",
-//           })
-//         );
-
-//         done();
-//       });
-//   });
-// });
-
 describe("/POST enroll to course", () => {
   it("it should enroll a user", (done) => {
     chai
@@ -114,6 +57,61 @@ describe("/POST enroll to course", () => {
       .end((err, res) => {
         chai.expect(err).to.be.null;
         chai.expect(res).to.have.status(400);
+        done();
+      });
+  });
+
+  it("it should create a user", (done) => {
+    chai
+      .request(server)
+      .post("/createuser")
+      .send({
+        email: "test@unic.ac.cy",
+        password: "tE5|tE5|",
+        name: "Test",
+        last: "Test",
+      })
+      .end((err, res) => {
+        chai.expect(err).to.be.null;
+        chai.expect(res).to.have.status(201);
+
+        const answer = JSON.parse(res.text);
+        if (answer.user_id) {
+          const theLink =
+            "/webservice/rest/server.php?wstoken=" +
+            process.env.DELETE_USER_TOKEN +
+            "&wsfunction=core_user_delete_users&moodlewsrestformat=json&userids[0]=" +
+            answer.user_id;
+
+          axios.delete(process.env.MOODLE_URL + theLink);
+        }
+
+        done();
+      });
+  });
+
+  it("if user already exists", (done) => {
+    chai
+      .request(server)
+      .post("/createuser")
+      .send({
+        email: "ivan0kosyakov@gmail.com",
+        password: "ivanIVAN1234@",
+        name: "Ivan",
+        last: "Ivan",
+      })
+      .end((err, res) => {
+        chai.expect(err).to.be.null;
+        chai.expect(res).to.have.status(200);
+        chai.expect(res.text).to.equal(
+          JSON.stringify({
+            status: true,
+            new_user: false,
+            user_id: 20,
+            message: "User Already Exist in moodle",
+          })
+        );
+
         done();
       });
   });
